e79d38e42a4f101cb068fabb68df944342a090e7
Lankesh <lankeshdesilva16@gmail.com>
2020-10-23 23:05:09 -0400

    pushing last changes

--- README.md
+++
@@ -1,2 +0,0 @@
-# User-Directory-
-React User Directory
---
+++ src/components/SiteHead/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./style.css";
+
+function SiteHead() {
+    return (
+<div className = "wrapper">
+    <div className = "title">
+Employee Directory
+    </div>
+    <br></br>
+    <div className = "tagline">
+Employee Profiles
+    </div>
+</div>
+
+    );
+}
+
+export default SiteHead;
